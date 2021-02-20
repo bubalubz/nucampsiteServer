63005f57397fa691177c1f3503fe58264199f2f3
Jeffrey Strayhorn <72161056+bubalubz@users.noreply.github.com>
2021-02-20 16:27:59 -0600

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
     'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'facebook': {
+        clientId: '237074614814189',
+        clientSecret: 'ebe6f21dcc82dff6b1909c37daf49766'
+    }
 }
